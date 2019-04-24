825f9db78c3f0e3701bffceeca632a07931a0157
Duanxiaodai <37828652+Duanxiaodai@users.noreply.github.com>
2019-04-24 16:35:41 +0800

    Create showMap.java

---
+++ Java数据结构与算法/Leetcode/data_structure/showMap.java
@@ -0,0 +1,15 @@
+import java.util.*;
+public class showMap {
+
+	   public static void main(String[] args) {
+		      Map m1 = new HashMap<Integer, Integer>(); 
+		      m1.put(1, 2);
+		      m1.put(1,2);
+		      m1.put(2, 1);
+		      m1.put(2, 3);
+		      System.out.println();
+		      System.out.println(" Map Elements");
+		      System.out.print("\t" + m1);			
+		   }
+
+}
