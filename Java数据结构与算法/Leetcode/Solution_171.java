af637506d0365bc3678d8eea12096babbcced3c7
Duanxiaodai <37828652+Duanxiaodai@users.noreply.github.com>
2019-04-29 21:42:22 +0800

    Create Solution_171.java

---
+++ Java数据结构与算法/Leetcode/Solution_171.java
@@ -0,0 +1,10 @@
+
+public class Solution_171 {
+    public int titleToNumber(String s) {
+        int sum = 0;
+        for (int i = 0;i<s.length();i++){
+            sum+=(s.charAt(i)-'A'+1)*Math.pow(26,s.length()-i-1);
+        }
+        return sum;
+    }
+}
